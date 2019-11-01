2f2c1a46778aeac5f32936a961f527516c854576
Paras Parmar <parasparmar@gmail.com>
2019-11-01 22:11:09 +0530

    Add project files.

---
+++ TryKnockout/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TryKnockout.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
