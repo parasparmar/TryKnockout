fc0e202a2708ac1a7991d75291ee56868c85824f
Paras Parmar <parasparmar@gmail.com>
2019-11-05 00:03:43 +0530

    Updated Sorting and Pagination

--- TryKnockout/Models/QueryOptions.cs
+++ TryKnockout/Models/QueryOptions.cs
@@ -15,6 +15,9 @@ namespace TryKnockout.Models
         }
         public string SortField { get; set; }
         public SortOrder SortOrder { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
 
         public string Sort
         {
