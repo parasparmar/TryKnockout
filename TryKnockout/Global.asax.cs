d680ce9bec4505f5fd5bc1337301e730d2a23a45
Paras Parmar <parasparmar@gmail.com>
2019-11-08 14:30:01 +0530

    page 86

--- TryKnockout/Global.asax.cs
+++ TryKnockout/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using TryKnockout.DAL;
+using System.Data.Entity;
 
 namespace TryKnockout
 {
@@ -16,6 +18,10 @@ namespace TryKnockout
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var bookContext = new BookContext();
+            Database.SetInitializer(new BookInitializer());
+            bookContext.Database.Initialize(true);
         }
     }
 }
---
+++ TryKnockout/Migrations/Configuration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+namespace TryKnockout.Migrations
+{
+
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TryKnockout.DAL.BookContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "TryKnockout.DAL.BookContext";
+        }
+
+        protected override void Seed(TryKnockout.DAL.BookContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+
+           
+        }
+    }
+}
